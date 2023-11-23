5805fe0e298cd6cbab34bbdfc2e5a312bc955627
Mohye <dragonsinmeliodas777@gmail.com>
2023-11-23 16:06:05 +0200

    adding login function in auth user controller

--- app/Http/Controllers/AuthUserContorller.php
+++ app/Http/Controllers/AuthUserContorller.php
@@ -41,4 +41,36 @@ class AuthUserContorller extends Controller
             'success' => 'user created successfully'
         ]);
     }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+
+            'email' =>  ['required', 'string', 'max:255', 'email'],
+            'password' => 'required'
+
+        ]);
+
+        if ($validator->fails()) {
+
+            return Response::json($validator->errors());
+        }
+
+        $credentials = $request->only('email', 'password');
+
+        try {
+            if (!$token = JWTAuth::attempt($credentials)) {
+
+                return Response::json(['error' => 'invalid user name and password'], [401]);
+            }
+        } catch (JWTException $e) {
+
+            return Response::json(['error' => 'could not create token'], [500]);
+        }
+
+        return response()->json([
+            'data' => compact('token'),
+            'message' => 'welcome back!!'
+        ]);
+    }
 }
