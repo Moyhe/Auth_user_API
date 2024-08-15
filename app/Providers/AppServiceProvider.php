0f25c1dfcfe7bf6bd274cfe79b566aea83f754bb
Mohye <dragonsinmeliodas777@gmail.com>
2024-08-16 02:27:49 +0300

    add deployment configuratoin files

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,7 +19,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url): void
     {
+
+        if (app()->environment() == 'production')
+            $url->forceScheme('https');
     }
 }
