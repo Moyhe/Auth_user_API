37d98ae38e14d18a7520233ae20b072d899502eb
Mohye <dragonsinmeliodas777@gmail.com>
2024-08-16 03:31:46 +0300

    add deployment configuratoin files

--- app/Http/Controllers/Auth/AuthUserContorller.php
+++ app/Http/Controllers/Auth/AuthUserContorller.php
@@ -21,66 +21,6 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 class AuthUserContorller extends Controller
 {
 
-    /**
-     * @OA\Schema(
-     *    schema="RegisterRequest",
-     *    @OA\Property(
-     *        property="name",
-     *        type="string",
-     *        description="User Name",
-     *        nullable=false,
-     *
-     *    ),
-     *    @OA\Property(
-     *        property="email",
-     *        type="string",
-     *        description="User EMail",
-     *        nullable=false,
-     *        format="email"
-     *    ),
-     *
-     *  *    @OA\Property(
-     *        property="password",
-     *        type="string",
-     *        description="User Password",
-     *        nullable=false,
-     *        example="password"
-     *    ),
-     * )
-     *
-     * @OA\Post(
-     *     path="/api/user/register",
-     *     tags={"users"},
-     *     summary="Authorize user",
-     *     description="register user",
-     *     operationId="register",
-     *     @OA\RequestBody(
-     *        @OA\JsonContent(ref="#/components/schemas/RegisterRequest")
-     *     ),
-     *     @OA\Response(
-     *         response=200,
-     *         description="Authentication successful",
-     *         @OA\JsonContent(
-     *             @OA\Property(
-     *                property="token",
-     *                type="string",
-     *                description="JWT authorization token",
-     *                example="1|fSPJ2AR0TU0dLB6aiYgtSGHkPnFTfBdh4ltISiSo",
-     *             ),
-     *             @OA\Property(
-     *                property="type",
-     *                type="string",
-     *                description="Token type",
-     *                example="bearer",
-     *             ),
-     *         )
-     *     ),
-     *     @OA\Response(
-     *         response=500,
-     *         description="Internal server error"
-     *     )
-     * )
-     */
 
     public function register(Request $request)
     {
@@ -110,59 +50,6 @@ class AuthUserContorller extends Controller
         return Response::json(compact('user', 'token'), 200);
     }
 
-
-    /**
-     * @OA\Schema(
-     *    schema="LoginRequest",
-     *    @OA\Property(
-     *        property="email",
-     *        type="string",
-     *        description="User EMail",
-     *        nullable=false,
-     *        format="email"
-     *    ),
-     *    @OA\Property(
-     *        property="password",
-     *        type="string",
-     *        description="User Password",
-     *        nullable=false,
-     *        example="password"
-     *    ),
-     * )
-     *
-     * @OA\Post(
-     *     path="/api/user/login",
-     *     tags={"users"},
-     *     summary="Authorize user",
-     *     description="Authorizes user by its email and password",
-     *     operationId="login",
-     *     @OA\RequestBody(
-     *        @OA\JsonContent(ref="#/components/schemas/LoginRequest")
-     *     ),
-     *     @OA\Response(
-     *         response=201,
-     *         description="Authentication successful",
-     *         @OA\JsonContent(
-     *             @OA\Property(
-     *                property="token",
-     *                type="string",
-     *                description="Sanctum authorization token",
-     *                example="1|fSPJ2AR0TU0dLB6aiYgtSGHkPnFTfBdh4ltISiSo",
-     *             ),
-     *             @OA\Property(
-     *                property="type",
-     *                type="string",
-     *                description="Token type",
-     *                example="bearer",
-     *             ),
-     *         )
-     *     ),
-     *     @OA\Response(
-     *         response=500,
-     *         description="Internal server error"
-     *     )
-     * )
-     */
 
     public function login(Request $request)
     {
